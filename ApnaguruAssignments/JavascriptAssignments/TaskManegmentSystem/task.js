d3f6beb6b63f56009cfd4134335dee04e07b6517
Your Name <rileash20@gmail.com>
2024-12-21 10:59:15 +0530

    m

---
+++ ApnaguruAssignments/JavascriptAssignments/TaskManegmentSystem/task.js
@@ -0,0 +1,13 @@
+let inputs1=document.getElementById("inp");
+let text=document.querySelector(".text");
+
+function Add()
+{
+    if(inputs1=="")
+    {
+        alert("Please enter task!!");
+    }
+    else{
+        
+    }
+}
